7f12a2538f58d413d70ea7186948d0a32e83d8e3
Francisco Manuel López García <manuel.lopez@resser.com>
2024-02-16 20:28:02 -0600

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 import "./styles.css";
+import { LoginPage } from "./auth/pages";
 
 const root = document.getElementById("root") as HTMLElement;
 
 const router = createHashRouter([
 	{
-	  path: "/*",
+	  path: "/",
 	  element: <App />,
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
 	}
   ]);
 
