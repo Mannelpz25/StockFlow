6ee18a396f6c91afff8012fbe8c0eff31344545a
Francisco Manuel López García <manuel.lopez@resser.com>
2024-02-16 20:14:42 -0600

    Change route to /home

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 import "./styles.css";
 
 const root = document.getElementById("root") as HTMLElement;
 
+const router = createHashRouter([
+	{
+	  path: "/*",
+	  element: <App />,
+	}
+  ]);
+
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
